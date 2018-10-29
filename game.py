b804a5eefe1501c5b78d56dfc9943cf85a871e5e
robin.kessels <robin.kessels@student.kuleuven.be>
2018-10-29 23:16:33 +0100

    added func to show text on screen

--- game.py
+++ game.py
@@ -1,4 +1,5 @@
 import pygame
+import time
 
 pygame.init()
 
@@ -9,31 +10,57 @@ black = (0, 0, 0)
 white = (255, 255, 255)
 red = (255, 0, 0)
 
-gameDisplay = pygame.display.set_mode((display_width, display_height))
+game_display = pygame.display.set_mode((display_width, display_height))
 pygame.display.set_caption('Snakepy')
 clock = pygame.time.Clock()
 
-dead = False
-while not dead:
 
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            dead = True
+def game_over():
+    message_display('Snake has died..')
+    game_loop()
 
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_UP:
-                direction = 'up'
-            elif event.key == pygame.K_DOWN:
-                direction = 'down'
-            elif event.key == pygame.K_LEFT:
-                direction = 'left'
-            elif event.key == pygame.K_RIGHT:
-                direction = 'right'
 
-    # move snake(direction)
-    # draw snake
+def message_display(text):
+    font = pygame.font.Font('freesansbold.ttf', 80)
+    text_surface, text_rect = text_objects(text, font)
+    text_rect.center = ((display_width/2), (display_height/2))
+    game_display.blit(text_surface, text_rect)
     pygame.display.update()
-    clock.tick(60)
+    time.sleep(2)
 
+
+def text_objects(text, font):
+    text_surface = font.render(text, True, white)
+    return text_surface, text_surface.get_rect()
+
+
+def game_loop():
+    game_exit = False
+    while not game_exit:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_UP:
+                    direction = 'up'
+                elif event.key == pygame.K_DOWN:
+                    direction = 'down'
+                elif event.key == pygame.K_LEFT:
+                    direction = 'left'
+                elif event.key == pygame.K_RIGHT:
+                    direction = 'right'
+
+        # move snake(direction)
+        # draw snake
+
+        # if x > display_width or x < 0:
+         #   game_over()
+        game_display.fill(black)
+        pygame.display.update()
+        clock.tick(60)
+
+
+game_loop()
 pygame.quit()
 quit()
